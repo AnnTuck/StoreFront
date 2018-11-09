ab33591f0297f8cd2b8074ff755768840c1e1432
Ann <anntuck@anntuck.com>
2018-11-09 17:02:11 -0500

    adds config information again

--- bamazon/server.js
+++ bamazon/server.js
@@ -1,19 +1,17 @@
 //server.js
 //Starting point for Node/Express server.
 
-
 //Dependencies
 const express = require('express');
 const path = require('path');
-
-//Sets up the Express app
-const app = express();
-
 //Specifies the port (Heroku or local)
 const PORT = process.env.PORT || 8080;
 
 //Require our models for syncing
 const db = require('./models');
+
+//Sets up the Express app
+const app = express();
 
 //Sets up middleware for Express app to handle data parsing
     //Parse application/x-www-form-urlencoded
@@ -21,13 +19,12 @@ const db = require('./models');
     //Parse application/json
     app.use(express.json());
     //Our public folder is the interface with the public.
-    app.use(express.static(path.join(__dirname,'public')));
+    app.use(express.static(path.join(__dirname,'./public')));
 
 //Routes
     
     require('./routes/department-api-routes.js')(app);
     require('./routes/product-api-routes.js')(app);
-    
     require('./routes/html-routes.js')(app);
 
 
@@ -46,10 +43,6 @@ const db = require('./models');
     //       });
     //     });
     // };
-
-
-
-
 
 
 //Sync our sequelize models then start our Express app, listen to PORT
